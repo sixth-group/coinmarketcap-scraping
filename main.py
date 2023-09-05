6ee75f02f1089b7ef60ba1bc6f57a0207c5dc852
samanmalekian <malekian@protonmail.com>
2023-09-05 13:25:17 +0330

    - Completed get_detail_of_coins. - Fixed some bugs and improved performance.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common import NoSuchElementException, StaleElementReferenceException
 
 
 class CoinMarketCap:
@@ -46,9 +47,63 @@ class CoinMarketCap:
                                            "/*[name()='use' and @href='#calendar']/parent::*/parent::*").click()
             self.driver.find_element(by=By.XPATH, value="//li[text()='Last 365 days']").click()
             self.driver.find_element(by=By.XPATH, value="//button[text()='Continue']").click()
+
+            time.sleep(2)
+
             self.driver.find_element(by=By.XPATH, value="//div[contains(text(), 'Download CSV')]").click()
 
-            time.sleep(1)
+            time.sleep(2)
+
+    def get_detail_of_coins(self, coins: list[dict]):
+        without_tag_coins = []
+        for coin in coins:
+            self.driver.get(coin['MainLink'])
+            self.driver.implicitly_wait(5)
+            self.driver.execute_script('window.scrollTo(0, 0);')
+
+            sidebar = self.driver.find_element(by=By.XPATH, value="//div[contains(@class, 'content_folded')]")
+
+            github_url = None
+            try:
+                github_url = sidebar.find_element(by=By.XPATH,
+                                                  value=".//span[text()='Official links']"
+                                                        "//following::div[1]"
+                                                        "//a[contains(text(), 'GitHub')]")
+                github_url = github_url.get_attribute('href')
+            except NoSuchElementException:
+                pass
+
+            tags = []
+            count_stale_exception = 0
+            while True:
+                if count_stale_exception >= 3:
+                    without_tag_coins.append(coin)
+                else:
+                    try:
+                        tags = sidebar.find_elements(by=By.XPATH,
+                                                     value=".//span[text()='Tags']"
+                                                           "//following::div[1]//a")
+                        tags = [{'tag': tag.text, 'url': tag.get_attribute('href')} for tag in tags if tags]
+                    except NoSuchElementException:
+                        pass
+                    except StaleElementReferenceException:
+                        count_stale_exception += 1
+                        print('waiting for StaleElementReferenceException handling...')
+
+                        time.sleep(30)
+
+                        self.driver.get(coin['MainLink'])
+                        self.driver.implicitly_wait(5)
+
+                        continue
+                break
+
+            coin['tags'] = tags
+            coin['github_url'] = github_url
+
+            time.sleep(2)
+
+        return coins, without_tag_coins
 
     @staticmethod
     def export_to_csv(filename: str, data: list, keys: list):
